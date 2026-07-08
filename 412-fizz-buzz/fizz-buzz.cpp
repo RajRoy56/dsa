6839918f090965b6ea29fa0c91b734eb5cec3c27
Rohan Roy <rajroyep@gmail.com>
2026-07-08 11:44:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (73.05%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -5,15 +5,12 @@ public:
         for(int i=1;i<=n;i++){
             if(i%3==0 && i%5==0){
                 ans.push_back("FizzBuzz");
-                continue;
             }
             else if(i%5==0){
                 ans.push_back("Buzz");
-                continue;
             }
             else if(i%3==0){
                 ans.push_back("Fizz");
-                continue;
             }
             else{
                 ans.push_back(to_string(i));
