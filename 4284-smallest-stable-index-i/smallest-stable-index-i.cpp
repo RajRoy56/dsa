c8cca6cf46545d9eb16411517013534e187b3ec7
Rohan Roy <rajroyep@gmail.com>
2026-09-04 22:18:27 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (46.94%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -4,17 +4,13 @@ public:
         int n=nums.size();
         int minm=INT_MAX;
         int maxm=nums[0];
-       // int maxi[n];
         vector<int>mini(n);
-        
-        // for(int i=0;i<n;i++){
-        //     maxm=max(maxm,nums[i]);
-        //     maxi[i] = maxm;
-        // }
+      
         for(int i=n-1;i>=0;i--){
             minm=min(minm,nums[i]);
             mini[i]=minm;
         }
+        
         for(int i=0;i<n;i++){
             if(maxm < nums[i]){
                 maxm = nums[i];
